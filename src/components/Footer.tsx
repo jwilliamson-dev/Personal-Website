5cf216b2abb423f0579237aed0257466707b8ef8
Jacob Williamson <me@jacobwilliamson.dev>
2022-03-21 15:01:13 -0400

    Completed footer, added CSS utility styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Grid, Typography, Box, Link } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import '../styles/utility.css'
 
 type Props = {}
 
-const Footer = (props: Props) => {
+const Footer: React.FC = (props: Props) => {
   return (
-    <div>Footer</div>
+    <Grid container className='banner-outer bottom' sx={{ bgcolor: 'primary.main' }}>
+      <Grid item sm={11} md={10} className='banner-inner'>
+        <Typography variant='body1'>
+          &copy; Copyright {(new Date()).getFullYear()} - Jacob Williamson, Powered by&nbsp;
+          <Link color='inherit' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>React</Link> 
+          &nbsp;and&nbsp;
+          <Link color='inherit' href='https://mui.com/' target='_blank' rel='noopener noreferrer'>MUI</Link>
+          , Website v1.0
+        </Typography>
+        <Box className='d-flex'>
+          <Link href='https://github.com/jwilliamson-dev' target='_blank' rel='noopener noreferrer'>
+            <GitHubIcon className='icon' />
+          </Link>
+          <Link href='https://www.linkedin.com/in/jjw324' target='_blank' rel='noopener noreferrer'>
+            <LinkedInIcon className='icon' />
+          </Link>
+          <Link href='https://twitter.com/jwilliamson_dev' target='_blank' rel='noopener noreferrer'>
+            <TwitterIcon className='icon' />
+          </Link>
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,14 @@ type Props = {};
 
 const Header: React.FC = (props: Props) => {
   return (
-    <Grid container
-      sx={{
-        bgcolor: 'primary.main',
-        justifyContent: 'center',
-        height: '3.5rem',
-        boxShadow: 3
-      }}>
-      <Grid item sm={11} md={10}
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center'
-        }}>
-        <Link variant="h4"color="inherit" underline="none" href="/">Jacob Williamson</Link>
+    <Grid container className='banner-outer' sx={{ bgcolor: 'primary.main', boxShadow: 3 }}>
+      <Grid item sm={11} md={10} className='banner-inner'>
+        <Link variant='h4'color='inherit' underline='none' href='/'>Jacob Williamson</Link>
         <Box>
-          <Button variant="text" color="secondary" href="/About">About</Button>
-          <Button variant="text" color="secondary" href="/Resume">Resume</Button>
-          <Button variant="text" color="secondary" href="/Projects">Projects</Button>
-          <Button disabled variant="text" href="/Blog">Blog</Button>
+          <Button variant='text' color='secondary' href='/About'>About</Button>
+          <Button variant='text' color='secondary' href='/Resume'>Resume</Button>
+          <Button variant='text' color='secondary' href='/Projects'>Projects</Button>
+          <Button disabled variant='text' href='/Blog'>Blog</Button>
         </Box>
       </Grid>
     </Grid>
