94f24b6c07b40beaf5b5c36c5ff96ffd219fa66f
Jacob Williamson <me@jacobwilliamson.dev>
2022-07-02 13:10:59 -0400

    Refactor home page to use useLocalData hook, remove unnecessary styled components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,58 +1,52 @@
-import { useEffect, useState } from 'react'
 import { Grid, Typography as Typ } from '@mui/material'
 import { TwitterTimelineEmbed } from 'react-twitter-embed'
-import { styled } from '@mui/material'
-import ActivityFeed from 'components/ActivityFeed'
 import { GitHubActivity } from 'types'
-
-const AFGrid = styled(Grid)`
-  max-height: 65vh;
-  overflow: auto;
-  margin-bottom: .5rem;
-`
-
-const Hello = styled(Typ)`
-  font-size: 3.5rem;
-  font-weight: 300;
-`
+import ActivityFeed from 'components/ActivityFeed'
+import useLocalData from 'hooks/useLocalData'
 
 const Home: React.FC = () => {
-  const setIsLoading = useState(false)[1]
-  const [gitHubData, setGitHubData] = useState<Array<GitHubActivity>>([])
-  
-  useEffect(() => {
-    const getGitHubData = async () => {
-      setIsLoading(true)
+  const {
+    data: gitHubData,
+    isLoading
+  } = useLocalData<GitHubActivity[]>('githubactivity.json')
 
-      try {
-        const res = await fetch('/data/githubactivity.json')
-        const jsonData: Array<GitHubActivity> = await res.json()
-        setGitHubData(jsonData)
-      } catch (error) {
-        console.error(error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    getGitHubData()
-  }, [])
+  const activityFeedProps = {
+    item: true,
+    xs: 10,
+    md: 5,
+    lg: 4,
+    maxHeight: '65vh',
+    overflow: 'auto',
+    mb: 1
+  }
   
   return (
     <>
-      <Grid item sm={11} md={10} height='fit-content' textAlign='center'>
-        <Hello>Hi, I'm Jacob!</Hello>
+      <Grid 
+        item 
+        sm={11} 
+        md={10} 
+        height='fit-content' 
+        textAlign='center'>
+        <Typ fontWeight={300} fontSize='3.5rem'>Hi, I'm Jacob!</Typ>
         <Typ variant='body2'>Using technology to automate and integrate</Typ>
       </Grid>
-      <AFGrid item xs={10} md={5} lg={4}>
-        <ActivityFeed title='My GitHub Feed' items={gitHubData} />
-      </AFGrid>
-      <AFGrid item xs={10} md={5} lg={4}>
+
+      <Grid {...activityFeedProps}>
+        { isLoading ? (
+          <Typ variant='h1'>Loading Data...</Typ>
+        ) : (
+          gitHubData &&
+            <ActivityFeed title='My GitHub Feed' items={gitHubData} />
+        ) }
+      </Grid>
+
+      <Grid {...activityFeedProps}>
         <TwitterTimelineEmbed
           sourceType="profile"
           screenName="jwilliamson_dev"
         ></TwitterTimelineEmbed>
-      </AFGrid>
+      </Grid>
     </>
   )
 }
